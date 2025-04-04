08af1453d0cce53d6329409fd36b6af71949c464
Niels Leenheer <niels@leenheer.nl>
2025-04-04 14:00:31 +0200

    Prepare for v2.0.1

--- dist/network-receipt-printer.mjs
+++ dist/network-receipt-printer.mjs
@@ -37,15 +37,28 @@ class NetworkReceiptPrinter extends ReceiptPrinterDriver {
 
         this.#options = {
 			host:		options.host || 'localhost',
-			port:		options.port || 9100
+			port:		options.port || 9100,
+			timeout:	options.timeout || 2500,
         };
 	}
 
 	async connect() {
+		this.#client.setTimeout(this.#options.timeout);
+
 		this.#client.connect(this.#options.port, this.#options.host, () => {
 			this.#emitter.emit('connected', {
 				type: 'network'
 			});
+		});
+
+		this.#client.on('timeout', () => {
+			this.#client.destroy();
+			this.#emitter.emit('timeout');
+		});
+
+		this.#client.on('error', (e) => {
+			this.#client.destroy();
+			this.#emitter.emit('error', e);
 		});
 
 		this.#client.on('close', () => {
@@ -63,11 +76,21 @@ class NetworkReceiptPrinter extends ReceiptPrinterDriver {
 
 	async disconnect() {
 		this.#client.destroy();
-		this.#emitter.emit('disconnected');
 	}
 	
 	async print(command) {
-		this.#client.write(command);
+		let CHUNK_SIZE = 1024;
+		let offset = 0;
+
+		while (offset < command.length) {
+			const chunk = command.slice(offset, offset + CHUNK_SIZE);
+			
+			await new Promise(resolve => {
+				this.#client.write(chunk, null, resolve);
+			});
+			
+			offset += CHUNK_SIZE;
+		}
 	}
 
 	addEventListener(n, f) {
