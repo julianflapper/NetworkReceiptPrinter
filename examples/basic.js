f39a8b0a3dfd38bd15b7f8ebb5177881e7d146cd
Niels Leenheer <niels@leenheer.nl>
2025-04-04 13:59:36 +0200

    Update examples

--- examples/basic.js
+++ examples/basic.js
@@ -1,23 +1,39 @@
 import NetworkReceiptPrinter from "../src/main.js";
 import ReceiptPrinterEncoder from "@point-of-sale/receipt-printer-encoder";
+import getPixels from 'get-pixels';
+
+let pixels = await new Promise(resolve => {
+    getPixels('image.png', (err, pixels) => {
+        resolve(pixels);
+    });
+});
 
 
 const printer = new NetworkReceiptPrinter({
-    host: '192.168.1.41',
+    host: '192.168.1.93',
     port: 9100
 });
 
 const encoder = new ReceiptPrinterEncoder({
-    language: 'esc-pos',
+    language: 'star-line',
+});
+
+printer.addEventListener('error', (e) => {
+    console.error('Connection error', e);
+});
+
+printer.addEventListener('timeout', (e) => {
+    console.error('Connection timed out');
 });
 
 printer.addEventListener('connected', async () => {
     console.log('Connected to printer');
 
-    const commands = encoder
+    let commands = encoder
         .initialize()
         .text('Hello, world!')
         .newline()
+        .image(pixels, 320, 320, 'atkinson')
         .newline()
         .cut()
         .encode();
